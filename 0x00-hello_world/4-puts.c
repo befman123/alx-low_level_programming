3fadfbaede0659e22b492ca06142dbec3a7030f4
befman123 <befman123@gmail.com>
2022-04-09 00:33:23 +0300

    Challenge 4 DONE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /* Prints a set of characters to the output stream */
-int main(void)
-{
-  char phrase_to_be_displayed[] = "\"Programming is like building a multilingual puzzle";
+int main(void){
+char phrase_to_be_displayed[] = "\"Programming is like building a multilingual puzzle";
 
-  puts (phrase_to_be_displayed);
+puts(phrase_to_be_displayed);
 
-  return(0);
+return (0);
 }
