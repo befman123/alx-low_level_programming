be54fa8f05e6d153225053e225e2ca1cf35cf682
befman123 <befman123@gmail.com>
2022-04-09 19:23:08 +0300

    Challenge 2 DONE

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ *Prints the alphabet in small letter followed
+ *by a new line
+ */int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+
+	return (0);
+}
