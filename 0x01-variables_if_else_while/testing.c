b198d16c278b72272aa61237f1f5de3869984b28
befman123 <befman123@gmail.com>
2022-04-12 16:20:03 +0300

    Small invisible changes

--- 0x01-variables_if_else_while/testing.c
+++ 0x01-variables_if_else_while/testing.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 /*
  *Print unique combinations of 3 of numbers 0-9
  */int main(void)
@@ -13,24 +14,24 @@
 			for (k = 48; k < 58; k++)
 				for (l = 48; l < 58; l++)
 				{
-					if ((i == j && l > k) || (k == l &&
-					j > i))
+					bool t1 = k + l > i + j;
+					bool t2 = k + l == i + j;
+					bool t3 = (i != j) && (k != l);
+				        bool t4 = t2 && t3;
+
+					if (i == 48 && j == 48 && k == 48 &&
+					    l == 48)
+						goto firsttime;
+					putchar(',');
+					putchar(' ');
+				firsttime:
+					if (t1 || t4)
 					{
 						putchar(i);
 						putchar(j);
 						putchar(' ');
 						putchar(k);
 						putchar(l);
-						if (i == 57 && j == 56)
-						{
-							if (k == 57 && j == 57)
-								continue;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
 				}
 	putchar('\n');
