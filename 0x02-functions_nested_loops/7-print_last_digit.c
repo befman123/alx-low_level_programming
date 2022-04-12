ed1a73035a853eb9a182da7590bd119c24a6a32d
befman123 <befman123@gmail.com>
2022-04-12 18:00:38 +0300

    Challenge 7 DONE

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ *Prints the last digit of a number
+ */int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+
+	return (a);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ void jack_bauer(void);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int n);
