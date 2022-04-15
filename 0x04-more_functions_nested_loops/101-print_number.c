430d342da5210b19ddabee8f35fb64e2d68c05fd
befman123 <befman123@gmail.com>
2022-04-15 21:45:21 +0300

    Challenge 12 DONE

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,19 +13,16 @@
 	else
 	{
 		if (n < 0)
-		{
-			n *= -1;
 			_putchar(45);
-		}
 		while (d != 0)
 		{
-			int div = n/d;
+			int div = n / d;
 
 			if (div != 0)
 				prnt_flag = 1;
 			if (prnt_flag == 1)
 			{
-				_putchar(n/d + '0');
+				_putchar((n / d) / -1 + '0');
 				n %= d;
 			}
 			d /= 10;
