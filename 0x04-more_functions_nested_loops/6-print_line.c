c94d47073b665210ce82156416710741d2f380f0
befman123 <befman123@gmail.com>
2022-04-15 16:15:27 +0300

    Challenge 6 DONE

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ *Prints line
+ */void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,9 +4,10 @@
 void _putchar(char);
 int _isupper(int);
 int _isdigit(int);
-int mul(int,int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
+void print_line(int);
 
 #endif
