3f1da5193d62fcaf74cf0997ad636693204640b0
befman123 <befman123@gmail.com>
2022-04-15 13:56:03 +0300

    Challenge 3 DONE

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ *Prints most numbers, between 0-9
+ */void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
