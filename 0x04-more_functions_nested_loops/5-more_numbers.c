2e8d3baea4cdbd5700d15e2def2273a2aed2129e
befman123 <befman123@gmail.com>
2022-04-15 14:32:29 +0300

    Challenge 5 DONE

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,18 @@
 
 	for (j = 0; j < 10; j++)
 	{
-		for (i = 0; i < 14; i++)
+		for (i = 0; i < 15; i++)
 		{
 			int w;
 
+			w = i;
 			if (i > 9)
+			{
 				w = w % 10;
-			if (i > 9)
 				_putchar(49);
+			}
 			_putchar(48 + w);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
