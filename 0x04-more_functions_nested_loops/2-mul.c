c90f80e0f33d2bc7781d120612b603d492f3bf91
befman123 <befman123@gmail.com>
2022-04-15 13:27:57 +0300

    Challenge 2 DONE

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/*
+ *Multiplies two integers
+ */int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 void _putchar(char);
 int _isupper(int);
 int _isdigit(int);
+int mul(int,int);
 
 #endif
