7e880f2edf24f81d10cd8dd9e15e97383b4398dd
befman123 <befman123@gmail.com>
2022-04-18 14:33:37 -0400

    Task #2 DONE

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ *Returns the length of a character array
+ */int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+		i++;
+
+	return (i);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,7 +3,9 @@
 
 #include <stdio.h>
 
+int _putchar(char);
 void reset_to_98(int *);
 void swap_int(int *, int *);
+int _strlen(char *);
 
 #endif /* MAIN_H */
