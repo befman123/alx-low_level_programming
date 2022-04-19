27456d70db607f5389d85ab1d1d6f9253c601cec
befman123 <befman123@gmail.com>
2022-04-19 08:49:05 +0300

    Task #5 DOOONEbetty 5-rev_string.c!

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ *Returns the length of a character array
+ */int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+		i++;
+
+	return (i);
+}
+/*
+ *Swap values
+ */void swap(char a, char b)
+{
+	char temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+/*
+ *Ammend in reverse
+ */void rev_string(char *c)
+{
+	int i;
+	int j;
+	char temp;
+
+	for (i = 0, j = 8; i < j; j--, i++)
+	{
+		if (i == j)
+			continue;
+		temp = *(c + i);
+		*(c + i) = *(c + j);
+		*(c + j) = temp;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ void reset_to_98(int *);
 void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *);
+void rev_string(char *s);
+void swap(char, char);
 
 #endif /* MAIN_H */
