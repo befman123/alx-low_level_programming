d7e24cbea724d9d9ae4e6977154025f2951f3354
befman123 <befman123@gmail.com>
2022-04-20 16:12:39 +0300

    Task #0 DONE

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/*
+ *Returns the length of a character array
+ */int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+		i++;
+
+	return (i);
+}
+/*
+ *Concatinates two string
+ */char *_strcat(char *dest, char *src)
+{
+	int i;
+	int destlen;
+
+	destlen = _strlen(dest);
+	i = 0;
+	while (*(src + i) != 0)
+	{
+		*(dest + destlen + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void _putchar(int);
+int _strlen(char *);
+char *_strcat(char *, char*);
+
+#endif
