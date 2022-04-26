31169d313e3050b2af13eb473e945397b70f4025
befman123 <befman123@gmail.com>
2022-04-26 16:04:50 +0300

    Task #2 DONE

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,27 @@
 #include "main.h"
 #include <stddef.h>
 /*
- *returns apointer to the first occurrence of
- *the character c
+ *Returns the length of a character array
+ */int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != 0)
+		i++;
+
+	return (i);
+}
+/*
+ *Return a pointer to the first occurrence of char c
  */char *_strchr(char *s, char c)
 {
 	int i;
+	int slen;
 
-	i = 0;
-	while (*(s + i) != 0)
-	{
+	slen = _strlen(s);
+	for (i = 0; i < slen; i++)
 		if (*(s + i) == c)
 			return (s + i);
-		i++;
-	}
 
 	return (NULL);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ void _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
+int _strlen(char *);
 
 #endif
