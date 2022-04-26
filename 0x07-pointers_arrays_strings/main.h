9a906fec2ca0311c93e5e13101cfd88af8de9b9e
befman123 <befman123@gmail.com>
2022-04-26 22:44:51 +0300

    Task #7 DONE

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,5 @@ unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
 void print_diagsums(int *, int);
-
+void print_chessboard(char (*)[8]);
 #endif
