9b7de47ac6e3622bbd215e7fd1ad083a307b507a
befman123 <befman123&gmail.com>
2022-06-14 20:55:58 +0300

    Chalenge #4 DONE!

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,26 @@
 #include "main.h"
 /*
- *Prints a string in reverse
- */void print_rev(char *c)
+ *Returns the length of a character array
+ */int _strlen(char *s)
 {
 	int i = 0;
 
-	while (*(c + i) != 0)
+	while (*(s + i) != 0)
 		i++;
 
-	for (; i >= 0; i--)
+	return (i);
+}
+/*
+ *Print a string in reverse
+ */void print_rev(char *S)
+{
+	int len;
+
+	len = _strlen(s);
+
+	while (len >= 0)
 	{
-		_putchar(*(c + i));
+		_putchar(*(s + len));
+		len--;
 	}
-	_putchar('\n');
 }
