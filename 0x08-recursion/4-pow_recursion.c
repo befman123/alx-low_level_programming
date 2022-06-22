788b7d0fffd10486d8ff0a4ca891fa66c564bb59
befman123 <befman123@gmail.com>
2022-06-22 21:44:39 +0300

    Challenge #4 DONE!

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ *Compute x raised to y
+ */int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
