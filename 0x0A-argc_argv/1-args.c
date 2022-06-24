682b03c1cd9fa513beec2887c9df810f6e1e8953
befman123 <befman123@gmail.com>
2022-06-24 17:01:36 +0300

    Challenge #1 DONE!

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /*
  *Print all the programs attributes
- */int main(int argc)
+ */int main(int argc, char *argv[])
 {
+	argv[argc - 1] = argv[argc - 3 + 2];
 	printf("%d\n", argc);
 	return (0);
 }
