b81f646ab667449d6602fecad03f713fe72eb897
befman123 <befman123@gmail.com>
2022-06-24 17:15:06 +0300

    Challenge #3 DONE!

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ *Prints the multiplication of two numberss
+ */int main(int argc, char *argv[])
+{
+	if (argc < 3)
+		printf("Error");
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
