fa76e5cc37986f6f698e8029971d79d9e811674a
befman123 <befman123@gmail.com>
2022-07-06 19:01:47 +0300

    Challenge #2 DONE

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/*
+ *Function that searches for integers
+ */int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL || array == NULL)
+		exit(98);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(*(array + i)) != 0)
+			return (i);
+	}
+	return (-1);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 void _putchar(char);
 void print_name(char*, void (*f)(char *));
 void array_iterator(int*, size_t, void (*action)(int));
+int int_index(int*, int, int (*cmp)(int));
 
 #endif
