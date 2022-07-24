8bd947c5ee683e5c4230e4ef16dfc4afd7c43fa9
befman123 <befman123@gmail.com>
2022-07-24 15:50:20 +0300

    Challenge #2 finally DONE!

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/*
+ *Returns the length of a string
+ */int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
+
+/*
+ *Concatinates two strings
+ */char *str_concat(char *s1, char *s2)
+{
+	int lens1, lens2, i;
+	char *ret;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	lens1 = _strlen_recursion(s1);
+	lens2 = _strlen_recursion(s2);
+	ret = (char *)malloc(lens1 + lens2 + 1);
+	for (i = 0; i < lens1; i++)
+	{
+		*(ret + i) = *(s1 + i);
+	}
+	for (i = 0; i < lens2 + 1; i++)
+	{
+		*(ret + i + lens1) = *(s2 + i);
+	}
+	return (ret);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,7 +3,8 @@
 
 void _putchar(char);
 char *create_array(unsigned int, char);
-char *_strdup(char*);
-char *str_concat(char *s1, char *s2);
+char *_strdup(char *);
+int _strlen_recursion(char *);
+char *str_concat(char *, char *);
 
 #endif
