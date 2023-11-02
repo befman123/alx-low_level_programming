210ba172108bd0e95576e2cf38be2bf956935292
befman123 <befman123@gamil.com>
2023-11-02 23:27:16 +0300

    Challenge #4 DONE!

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,10 +13,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == 0 && ptr != NULL)
-	{
 		free(ptr);
-		return (NULL);
-	}
 	if (ptr == NULL)
 		return (malloc(new_size));
 	if (new_size == old_size)
