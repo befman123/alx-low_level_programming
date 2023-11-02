642d057ebe11d2396f7e3e933cccb5040bcc1114
befman123 <befman123@gamil.com>
2023-11-02 16:02:36 +0300

    Added a readme and header files

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory like malloc
+ * @b: The amount of memory allocated
+ *
+ *
+ * Return: a void pointer on success or exits with code 98
+ *         on failure
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = NULL;
+	if (b > 0)
+	{
+		ptr = malloc(b);
+		if (ptr)
+			return (ptr);
+	}
+	else
+		exit(98);
+	return (ptr);
+}
